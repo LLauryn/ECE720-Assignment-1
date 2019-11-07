99e5dec7571b005a26b280ab43d4009ebcff4f48
ylou2 <ylou2@uadls.ad.ualberta.ca>
2019-11-07 13:27:41 -0700

    giant components changed

--- RCommands/720_1.R
+++ RCommands/720_1.R
@@ -1,18 +1,33 @@
+
+install.packages("SNA")
+library(SNA)
+install.packages("igraph")
+library(igraph)
+install.packages("CINNA")
+library(CINNA)
+
+
 #data = read.table(file='Desktop/720assign1/data/allPosts.tsv',sep='\t',header=TRUE)
 #data = read.table(file='Desktop/720assign1/data/allPosts-metaData.tsv',sep='\t',header=TRUE)
-data = as.matrix(read.table(file='Desktop/720assign1/data/askerAnswer.tsv',sep='\t',header=TRUE))
+#data = as.matrix(read.table(file='Desktop/720assign1/data/askerAnswer.tsv',sep='\t',header=TRUE))
+
 gplot(data)
-deg = degree(data)
-x = sort(x=deg,decreasing = TRUE)
-N = 12
-s = {}
-index = order(deg,decreasing = TRUE)[1:N]
-for (i in 1:N){
-  a = which(data[index[i],]!=0)
-  b = which(data[,index[i]]!=0)
-  s = union(s,union(a,b))
+
+graph = graph_from_adjacency_matrix(data, mode = "directed")
+giant_components = giant_component_extract(graph, directed = TRUE)
+plot(giant_components[[1]],vertex.label = NA,vertex.size = 7,edge.arrow.size = 0.5)
+
+a = giant_components[[2]]
+s={}
+
+for (i in 1:length(a[,1])){
+  for (j in 1:length(a[1,])){
+    x = gsub("X","",a[i,j])
+    y = as.numeric(as.character(x))
+    s = union(s,y)
+  }
+ 
 }
-s = union(index,s)
 giant_components = data[s,s]
-gplot(giant_components)
-write.table(giant_components,file='Desktop/720assign1/data/asker-answerer-giant.tsv',sep="\t")
+#gplot(giant_components)
+write.table(giant_components,file="C:/Users/ylou2/Desktop/data/asker-answerer-giant.tsv",sep="\t")
